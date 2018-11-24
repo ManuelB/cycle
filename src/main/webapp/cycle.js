f0fe7ed370d883d8c27ea18eceafb7ba169cd585
Lars Jung <lars.jung@students.hs-mainz.de>
2018-11-24 12:22:56 +0100

    start and end point through clicking

--- src/main/webapp/cycle.js
+++ src/main/webapp/cycle.js
@@ -1,6 +1,8 @@
 var Cycle = function() {
     var me = this;
     this.mapBoxToken = 'pk.eyJ1IjoibWFudWVsYjg2IiwiYSI6ImNqaXNydnhoZjIwYW4zcHA4cjV1OTJlbWUifQ.dvyaW4atK652Sor1113oDg';
+    this.start = null;
+    this.end = null;
     this.initMapBox();
 };
 Cycle.prototype.initMapBox = function() {
@@ -15,12 +17,31 @@ Cycle.prototype.initMapBox = function() {
     this.map.on('load', function() {
         me.onMapLoad();
     });
+    this.map.on('click', function(e) {
+    	me.onMapClick(e);
+    });
 };
 Cycle.prototype.onMapLoad = function() {
 	var me = this;
+	
+	// Do nothing for now
+};
+Cycle.prototype.onMapClick = function(e) {
+	var me = this;
+	if (this.start === null) {
+		this.start = e.lngLat;
+	} else {
+		this.end = e.lngLat;
+		me.loadRouteLayer();
+		this.start = null;
+		this.end = null;
+	}
+};
+Cycle.prototype.loadRouteLayer = function() {
+	var me = this;
 	this.map.addSource('route', {
 		type: 'geojson',
-		data: 'http://localhost:8080/cycle/resources/route'
+		data: 'http://localhost:8080/cycle/resources/routing/route?startLon=' + start.lng + '&startLat=' + start.lat + '&endLon=' + end.lng + '&endLat=' + end.lat
 	});
     this.map.addLayer({
         "id": "route",
